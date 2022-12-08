b21d8a80c261419f3191d472be7c41360cb1e62b
Melaku <melakuwondwossen111@gmail.com>
2022-12-08 11:01:54 -0900

    Exercises and Building a small calculator

---
+++ Calculator.py
@@ -0,0 +1,35 @@
+import sys
+from functools import partial
+# ...
+
+def evaluateExpression(expression):
+    # ...
+
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
--- button_holder.py
+++ button_holder.py
@@ -1,9 +1,26 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton
-class ButtonHolder(QMainWindow):
-    def __init__ (self):
-        super().__init__()
-        self.setWindowTitle("Test app")
-        button = QPushButton("Test")
-
-    #Set up the button as our central widget
-        self.setCentralWidget(button)
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QPushButton,
+    QWidget,
+)
+app = QApplication([])
+window = QWidget()
+layout = QGridLayout()
+layout.addWidget(QPushButton("Button (0, 0)"), 0, 0)
+layout.addWidget(QPushButton("Button (0, 1)"), 0, 1)
+layout.addWidget(QPushButton("Button (0, 2)"), 0, 2)
+layout.addWidget(QPushButton("Button (1, 0)"), 1, 0)
+layout.addWidget(QPushButton("Button (1, 1)"), 1, 1)
+layout.addWidget(QPushButton("Button (1, 2)"), 1, 2)
+layout.addWidget(QPushButton("Button (2, 0)"), 2, 0)
+layout.addWidget(
+    QPushButton("Button (2, 1) + 2 Columns Span"), 2, 1, 1, 2
+)
+window.setLayout(layout)
+window.setGeometry(100, 100, 280, 80)
+helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
+helloMsg.move(60, 15)
+window.show()
+sys.exit(app.exec())
